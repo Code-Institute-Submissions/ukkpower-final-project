cb146a53903bd9753387a629fc8b3fcd872c7cbe
ukkpower <ukkpower@gmail.com>
2021-09-27 21:11:44 +0000

    added membership template

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def membership(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/membership.html')
