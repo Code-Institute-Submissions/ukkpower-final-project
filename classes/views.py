29a211648a11048c7e4ebf1585e096e347c3306f
ukkpower <ukkpower@gmail.com>
2021-09-29 19:45:25 +0000

    added: classes view

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import GymClass
 
-# Create your views here.
+
+def classes(request):
+    """ A view to return the index page """
+
+    gym_classes = GymClass.objects.all()
+
+    context = {
+        'gym_classes': gym_classes,
+    }
+
+    return render(request, 'classes/classes.html', context)
--- home/views.py
+++ home/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-def membership(request):
-    """ A view to return the index page """
-
-    return render(request, 'home/membership.html')
