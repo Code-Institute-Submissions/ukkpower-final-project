547e38a43568f89e9c217bbaeb5aca9b268c47e6
ukkpower <ukkpower@gmail.com>
2021-09-30 22:14:36 +0000

    added: add to cart functionality

--- cart/views.py
+++ cart/views.py
@@ -9,3 +9,23 @@ def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    gym_class = get_object_or_404(GymClass, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+        messages.success(request, f'Updated {gym_class.name} quantity to {cart[item_id]}')
+    else:
+        cart[item_id] = quantity
+        messages.success(request, f'Added {gym_class.name} to your bag')
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
