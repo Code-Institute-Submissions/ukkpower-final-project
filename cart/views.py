1d98275f69f9037e9f72fda0671f6e050d5f3438
ukkpower <ukkpower@gmail.com>
2021-10-02 23:25:30 +0000

    added: update and delete cart items

--- cart/views.py
+++ cart/views.py
@@ -29,3 +29,40 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+def update_cart(request, item_id):
+    """Adjust the quantity of the specified gym class to the specified amount"""
+
+    gym_class = get_object_or_404(GymClass, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id] = quantity
+        messages.success(request, f'Updated {gym_class.name} quantity to {cart[item_id]}')
+    else:
+        cart.pop(item_id)
+        messages.success(request, f'Removed {gym_class.name} from your cart')
+    messages.success(request, f'nothing to see')
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
+
+
+def delete_cart_item(request, item_id):
+    """Remove the item from the cart"""
+
+    try:
+        gym_class = get_object_or_404(GymClass, pk=item_id)
+
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+        messages.success(request, f'Removed {gym_class.name} from your cart')
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
