292e9a17f1b7d3f519bd0d64182948eaa9a295be
ukkpower <ukkpower@gmail.com>
2021-10-12 22:30:57 +0000

    update: added classes

--- classes/models.py
+++ classes/models.py
@@ -25,12 +25,15 @@ class GymClass(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = HTMLField()
+    short = models.TextField(blank=True)
     level = models.CharField(max_length=254)
     duration = models.IntegerField()
     period = models.CharField(max_length=254)
     students = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     trainer = models.ManyToManyField('Trainer', blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
