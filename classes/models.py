61a4c9bd98bfc08a52fb1a934789ddfd270bbfaf
ukkpower <ukkpower@gmail.com>
2021-11-16 21:56:24 +0000

    fix: removed PEP8 format errors

--- classes/models.py
+++ classes/models.py
@@ -6,12 +6,12 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-      
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def get_friendly_name(self):
         return self.friendly_name
@@ -22,7 +22,8 @@ class GymClass(models.Model):
     class Meta:
         verbose_name_plural = 'GymClasses'
 
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = HTMLField()
     short = models.TextField(blank=True)
@@ -36,7 +37,7 @@ class GymClass(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Trainer(models.Model):
@@ -47,7 +48,7 @@ class Trainer(models.Model):
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Timetable(models.Model):
