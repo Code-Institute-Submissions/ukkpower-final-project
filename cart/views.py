8d9326463bd30f1265b61175c8b91e91e611c209
ukkpower <ukkpower@gmail.com>
2021-11-16 20:06:51 +0000

    fix: removed PEP8 format errors

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, HttpResponse, \
+    get_object_or_404
 from django.contrib import messages
 
 from classes.models import GymClass
 
-# Create your views here.
 
 def view_cart(request):
     """ A view that renders the cart contents page """
@@ -21,8 +21,10 @@ def add_to_cart(request, item_id):
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(request, f'Updated {gym_class.name} quantity to {cart[item_id]}')
+        cart[item_id] = quantity
+        messages.success(
+            request, f"Updated {gym_class.name} quantity to {cart[item_id]}"
+        )
     else:
         cart[item_id] = quantity
         messages.success(request, f'Added {gym_class.name} to your bag')
@@ -30,8 +32,9 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
     return redirect(redirect_url)
 
+
 def update_cart(request, item_id):
-    """Adjust the quantity of the specified gym class to the specified amount"""
+    """Adjust the quantity of the gym class to the specified amount"""
 
     gym_class = get_object_or_404(GymClass, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -40,11 +43,12 @@ def update_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id] = quantity
-        messages.success(request, f'Updated {gym_class.name} quantity to {cart[item_id]}')
+        messages.success(
+            request, f"Updated {gym_class.name} quantity to {cart[item_id]}"
+        )
     else:
         cart.pop(item_id)
         messages.success(request, f'Removed {gym_class.name} from your cart')
-    messages.success(request, f'nothing to see')
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
 
