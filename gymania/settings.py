b8ed6c2f8b18b87bab5dcb20c9b3b881a7618efc
ukkpower <ukkpower@gmail.com>
2021-11-16 22:09:37 +0000

    fix: removed PEP8 format errors

--- gymania/settings.py
+++ gymania/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1=-5cjctsd^(u=c22w08n(s#4)s58%u44+*673pt_*6zk&8$po"
+SECRET_KEY = (
+    "django-insecure-1=-5cjctsd^(u=c22w08n(s#4)s58%u44+*673pt_*6zk&8$po")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -124,7 +125,8 @@ WSGI_APPLICATION = 'gymania.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse("postgres://uskiqgmbdtnxbh:908617221ca7212034d72a5770e7da518426a038487c749083ec1cadb5b80cc7@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/dahbe7np52kuim")
+    'default': dj_database_url.parse(
+        "postgres://uskiqgmbdtnxbh:908617221ca7212034d72a5770e7da518426a038487c749083ec1cadb5b80cc7@ec2-34-247-151-118.eu-west-1.compute.amazonaws.com:5432/dahbe7np52kuim")
 }
 
 
@@ -133,7 +135,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': ('django.contrib.auth.password_validation.UserAttributeSimilarityValidator'),
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -217,9 +219,9 @@ else:
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
-  
+
 TINYMCE_DEFAULT_CONFIG = {
-    "plugins": "code",  
+    "plugins": "code",
 }
 
 
