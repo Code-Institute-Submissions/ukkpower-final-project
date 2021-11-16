be0aa102cb205e50fa5f7fbc528af350e86b2d09
ukkpower <ukkpower@gmail.com>
2021-11-16 21:44:52 +0000

    fix: removed PEP8 format errors

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,14 +1,13 @@
+import json
+import time
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from .models import Order, OrderLineItem
 from classes.models import GymClass
 from profiles.models import UserProfile
-
-import json
-import time
+from .models import Order, OrderLineItem
 
 
 class StripeWH_Handler:
@@ -69,8 +68,10 @@ class StripeWH_Handler:
                 profile.default_phone_number = shipping_details.phone
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address1 = (
+                    shipping_details.address.line1)
+                profile.default_street_address2 = (
+                    shipping_details.address.line2)
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -127,14 +128,6 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                gym_class=gym_class,
-                                quantity=quantity,
-                            )
-                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
