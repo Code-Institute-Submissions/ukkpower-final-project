f0512f4c90b86389862cea8281ac4a0bccfc6e3a
ukkpower <ukkpower@gmail.com>
2021-11-16 20:10:49 +0000

    fix: removed PEP8 format errors

--- checkout/admin.py
+++ checkout/admin.py
@@ -15,7 +15,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 
+              'email', 'phone_number',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county',
               'order_total', 'original_cart',
