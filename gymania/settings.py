590e9168236ec6af359c64da15c39c9756be273c
ukkpower <ukkpower@gmail.com>
2021-11-18 21:42:15 +0000

    update: secret key now comes from environ variables

--- gymania/settings.py
+++ gymania/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-1=-5cjctsd^(u=c22w08n(s#4)s58%u44+*673pt_*6zk&8$po")
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
