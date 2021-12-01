57d6b2cfdf46b27b21c620df95589b950da42a81
ukkpower <ukkpower@gmail.com>
2021-12-01 17:23:03 +0000

    added: pre pop inputs for time and day

--- classes/forms.py
+++ classes/forms.py
@@ -29,6 +29,20 @@ class TimetableForm(forms.ModelForm):
     class Meta:
         model = Timetable
         fields = '__all__'
+        widgets = {
+            'day': forms.Select(
+                choices=[('Monday', 'Monday'), ('Tuesday', 'Tuesday'),
+                         ('Wednesday', 'Wednesday'), ('Thursday', 'Thursday'),
+                         ('Friday', 'Friday'), ('Saturday', 'Saturday'),
+                         ('Sunday', 'Sunday')]),
+            'time_slot': forms.Select(
+                choices=[('9:00 am', '9:00 am'), ('10:00 am', '10:00 am'),
+                         ('11:00 am', '11:00 am'), ('12:00 pm', '12:00 pm'),
+                         ('1:00 pm', '1:00 pm'), ('2:00 pm', '2:00 pm'),
+                         ('3:00 pm', '3:00 pm'), ('4:00 pm', '4:00 pm'),
+                         ('5:00 pm', '5:00 pm'), ('6:00 pm', '6:00 pm'),
+                         ('7:00 pm', '7:00 pm')]),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
